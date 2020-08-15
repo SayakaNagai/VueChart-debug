8d83462f5c746963cfef2eb987f6e4751201d232
長井沙也伽 <sayaka_nagai@nagaisayatoginoMacBook-Air.local>
2020-08-15 21:50:26 +0900

    Add graph

---
+++ front/components/BarChart.vue
@@ -0,0 +1,10 @@
+<script>
+import { Bar } from 'vue-chartjs'
+export default {
+  extends: Bar,
+  props: ['chartData', 'options'], // propsに chartData と options を定義
+  mounted() {
+    this.renderChart(this.chartData, this.options)
+  }
+}
+</script>
--- front/pages/inspire.vue
+++ front/pages/inspire.vue
@@ -1,15 +1,53 @@
 <template>
-  <v-layout>
-    <v-flex text-xs-center>
-      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-flex>
-  </v-layout>
+  <!-- chartData に代入をしている -->
+  <v-row>
+    <v-col :cols="3">
+      <MainChart
+        class="chart"
+        :option="{ maintainAspectRatio: false }"
+        :chartData="chartData"
+        :width="100"
+        :height="100"
+      />
+    </v-col>
+  </v-row>
 </template>
+
+<script>
+import MainChart from '@/components/BarChart'
+export default {
+  data() {
+    return {
+      chartData: {
+        labels: ['January', 'February', 'March', 'April', 'May'],
+        datasets: [
+          {
+            label: 'Sample1',
+            data: [28, 20, 30, 40, 50],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+          }
+        ]
+      }
+    }
+  },
+  components: {
+    MainChart
+  }
+}
+</script>
